e831a9f844251f84058b56008e7f15a2f0211770
Jason Li <jason.li0102@gmail.com>
2018-11-22 14:29:13 -0800

    Add output method to output data to csv file

--- src/index.js
+++ src/index.js
@@ -36,6 +36,20 @@ class UserFunctions {
 
     return this.data[tableName].map(row => deleteProperty(row, columnName))
   }
+
+  output(dir, tableName) {
+    if (!this.data[tableName]) {
+      throw new Error(`Table ${tableName} does not exist`);
+    }
+
+    const data = PapaParse.unparse(this.data[tableName])
+    const file = `${dir}/${tableName}.csv`
+
+    fs.writeFile(file, data, (err) => {
+      if (err) throw err;
+      console.log(`The file ${tableName}.csv has been saved to ${dir}!`);
+    });
+  }
 }
 
 const addProperty = (row, newColumnName, transformer) => {
