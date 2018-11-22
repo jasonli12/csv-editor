03251d648eea681cd005aa42c04519cfb5cb2689
Jason Li <jason.li0102@gmail.com>
2018-11-22 12:05:17 -0800

    Add createNewCol method

--- src/index.js
+++ src/index.js
@@ -13,14 +13,24 @@ class UserFunctions {
       header: true,
       dynamicTyping: true,
       complete: (results) => {
-      	console.log("Parsing complete:", results);
-        console.log(tableName);
         this.data[tableName] = results.data;
       }
     })
   }
 
+  createNewCol(tableName, newColumnName, transformer) {
+    if (!this.data[tableName]) {
+      throw new Error(`Table ${tableName} does not exist`);
+    }
+    this.data[tableName].map(row => addProperty(row, newColumnName, transformer))
+  }
 }
+
+const addProperty = (row, newColumnName, transformer) => {
+  typeof transformer === 'function' ? row[newColumnName] = transformer(row) : row[newColumnName] = transformer;
+  return row;
+}
+
 module.exports = {
   UserFunctions: UserFunctions
 };
